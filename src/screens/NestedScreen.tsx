a8e607019740d73dd0bf8307bf1c4d49ff8798cd
Shreyas Papinwar <spapinwar@gmail.com>
2023-07-11 01:49:08 +0530

    Adding zoom to media viewing

--- src/screens/NestedScreen.tsx
+++ src/screens/NestedScreen.tsx
@@ -1,17 +1,21 @@
 import React from "react";
-import { StyleSheet, Text, View, ActivityIndicator } from "react-native";
+import { StyleSheet, Text, View, ActivityIndicator, Image } from "react-native";
 import { isLightTheme } from "../utils/colorScheme";
 import { ImageZoom } from "@likashefqet/react-native-image-zoom";
 import { Box } from "../components/core";
-import ImageViewer from 'react-native-image-zoom-viewer';
 import Modal from "react-native-modal";
 import FastImage from "react-native-fast-image";
 import { createImageProgress } from "react-native-image-progress";
+import { ReactNativeZoomableView } from '@openspacelabs/react-native-zoomable-view';
 
 type NestedScreenProps = {
   route: {
     params: {
-      msg: string;
+      msg: {
+        image: {
+          uri: string;
+        };
+      }
     };
   };
 };
@@ -34,7 +38,26 @@ const renderLoading = () => {
 }
   return (
     <Box h="100%" w="100%" backgroundColor={isLightTheme ? "white": "black"}>
-      <ImageViewer
+      <ReactNativeZoomableView
+          maxZoom={30}
+          // Give these to the zoomable view so it can apply the boundaries around the actual content.
+          // Need to make sure the content is actually centered and the width and height are
+          // dimensions when it's rendered naturally. Not the intrinsic size.
+          // For example, an image with an intrinsic size of 400x200 will be rendered as 300x150 in this case.
+          // Therefore, we'll feed the zoomable view the 300x150 size.
+          contentWidth={500}
+          contentHeight={500}
+        >
+          {
+            console.log(route.params.msg.image)
+          }
+          <Image
+            style={{ width: '100%', height: '100%' }}
+            source={{ uri: "ph://9F983DBA-EC35-42B8-8773-B597CF782EDD/L0/001/IMG_0003.JPG" }}
+          />
+        </ReactNativeZoomableView>
+
+      {/* <ImageViewer
                 enablePreload={true}
                 imageUrls={[{ url: route.params.msg.image.uri }]}
                 useNativeDriver={true}
@@ -42,7 +65,7 @@ const renderLoading = () => {
                 renderImage={renderImage}
                 loadingRender={renderLoading}
                 saveToLocalByLongPress={false}
-             />
+             /> */}
 
         {/* <ImageZoom
           uri={route.params.msg.image.uri}
